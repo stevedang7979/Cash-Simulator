71ae984e1ca3ccdfaaa28af3d1151f2fb19be33d
stevedang7979 <94345466+stevedang7979@users.noreply.github.com>
2021-12-28 04:13:17 -0500

    Delete lru.h

--- lru.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LRU_H
-#define __LRU_H
-#include "cache.h"
-
-void lru_init(Cache *cache);
-void lru_destroy(Cache *cache);
-void lru_fetch(Set *set, unsigned int tag, LRUResult *result);
-
-#endif
